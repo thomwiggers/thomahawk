a9d70a7045f214ee1d85d1af7d96638aa28bf3d0
theguyofdoom <theguyofdoom@fbf1ad31-3caa-4afa-b5b4-094426959cba>
2008-07-12 13:24:02 +0000

    git-svn-id: http://subversion.assembla.com/svn/thomahawk@17 fbf1ad31-3caa-4afa-b5b4-094426959cba

--- Trunk/libs/Thomahawk/Log.php
+++ Trunk/libs/Thomahawk/Log.php
@@ -31,11 +31,11 @@ class Thomahawk_Log extends Zend_Log {
 		parent::__construct();
 		
 		try{
-		require_once('Zend/Config/Ini.php');
-		$ini_log = new Zend_Config_Ini($path_to_root . 'conf/logging.ini');
-		$this->_prioriteit= $ini_log->log->prioriteit;
-		$this->_logView = $ini_log->log->view;
-		$this->_logEdit = $ini_log->log->edit;
+			require_once('Zend/Config/Ini.php');
+			$ini_log = new Zend_Config_Ini($path_to_root . 'conf/logging.ini');
+			$this->_prioriteit= $ini_log->log->prioriteit;
+			$this->_logView = $ini_log->log->view;
+			$this->_logEdit = $ini_log->log->edit;
 		}catch (Zend_Config_Exception $e){
 			throw new Thomahawk_LogException('Zend_Config_Ini maakte een fout/kon de INI niet vinden: ' . $e);
 		}catch (Exception $e){
@@ -43,36 +43,36 @@ class Thomahawk_Log extends Zend_Log {
 		}
 		try {
 			
-		require_once('Zend/Log/Filter/Priority.php');
-		require_once('Zend/Log/Writer/Stream.php');
-		require_once('Zend/Log/Formatter/Xml.php');
+			require_once('Zend/Log/Filter/Priority.php');
+			require_once('Zend/Log/Writer/Stream.php');
+			require_once('Zend/Log/Formatter/Xml.php');
 		
-		//extra loggers
-		$this->setEventItem('user', $user);
+			//extra loggers
+			$this->setEventItem('user', $user);
 		
-		//filter
-		$filter = new Zend_Log_Filter_Priority((int) $this->_prioriteit);
+			//filter
+			$filter = new Zend_Log_Filter_Priority((int) $this->_prioriteit);
 		
-		//normale writer maken
-		$wr1 = new Zend_Log_Writer_Stream($path_to_root . 'logs/Log.log');
-		$wr1->addFilter($filter);
-		$wr1->setFormatter(new Zend_Log_Formatter_Xml());
-		$this->addWriter($wr1);
+			//normale writer maken
+			$wr1 = new Zend_Log_Writer_Stream($path_to_root . 'logs/Log.log');
+			$wr1->addFilter($filter);
+			$wr1->setFormatter(new Zend_Log_Formatter_Xml());
+			$this->addWriter($wr1);
 
-		//error writer
-		$wr2 = new Zend_Log_Writer_Stream($path_to_root . 'logs/Errorlog.log');
-		$filter2 = new Zend_Log_Filter_Priority(8, '=');
-		$formatter2 = new Zend_Log_Formatter_Simple('%timestamp% %priorityName%(%priority%): %message%' . PHP_EOL. "%trace%" . PHP_EOL. PHP_EOL);
-		$wr2->addFilter($filter2);
-		$wr2->setFormatter($formatter2);
-		$this->addWriter($wr2);
+			//error writer
+			$wr2 = new Zend_Log_Writer_Stream($path_to_root . 'logs/Errorlog.log');
+			$filter2 = new Zend_Log_Filter_Priority(8, '=');
+			$formatter2 = new Zend_Log_Formatter_Simple('%timestamp% %priorityName%(%priority%): %message%' . PHP_EOL. "%trace%" . PHP_EOL. PHP_EOL);
+			$wr2->addFilter($filter2);
+			$wr2->setFormatter($formatter2);
+			$this->addWriter($wr2);
 		
-		//opruimen
-		unset($ini_log);
-		}catch (Exception $e){
-			throw new Thomahawk_LogException($e);
+			//opruimen
+			unset($ini_log);
+			}catch (Exception $e){
+				throw new Thomahawk_LogException($e);
+			}
 		}
-	}
 	function logException($exception){
         // sanity checks
         $priority = 8;  //EXCEP
